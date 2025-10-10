4505fbc543d2159aa5568fbbbd268525bb63582f
Harsh M <52977247+harshmriduhash@users.noreply.github.com>
2025-10-10 23:56:21 +0530

    handeling state in InputIdea

--- src/pages/inputidea.jsx
+++ src/pages/inputidea.jsx
@@ -12,7 +12,6 @@ export default function IdeaInputAssistant() {
 
   const navigate = useNavigate();
   const [AIresponse , setAIResponse] = useState(null);
-
   const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY);
   const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash-lite" }); 
 
