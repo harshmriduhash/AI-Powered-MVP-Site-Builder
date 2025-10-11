1b1268bda0aeaf161e0d6adf5248d9faa7538245
Harsh M <52977247+harshmriduhash@users.noreply.github.com>
2025-10-11 23:26:25 +0530

    Refactor GoogleAccountDeletion service for improved code formatting and readability by organizing imports and adjusting function definition spacing.

--- src/services/googleaccountdeletion.js
+++ src/services/googleaccountdeletion.js
@@ -1,13 +1,16 @@
-import { GoogleAuthProvider, reauthenticateWithPopup, deleteUser } from "firebase/auth";
+import {
+  GoogleAuthProvider,
+  reauthenticateWithPopup,
+  deleteUser,
+} from "firebase/auth";
 import { doc, deleteDoc } from "firebase/firestore";
 import { auth, db } from "../firebase";
 
-const GoogleAccountDeletion =async () => {
- const user = auth.currentUser;
+const GoogleAccountDeletion = async () => {
+  const user = auth.currentUser;
   const provider = new GoogleAuthProvider();
 
   try {
-
     await reauthenticateWithPopup(user, provider);
     await deleteDoc(doc(db, "users", user.uid));
     await deleteUser(user);
@@ -16,6 +19,6 @@ const GoogleAccountDeletion =async () => {
   } catch (err) {
     console.error("Error deleting Google account:", err);
   }
-}
+};
 
-export default GoogleAccountDeletion
+export default GoogleAccountDeletion;
