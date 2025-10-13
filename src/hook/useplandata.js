f289abaea8841882753ec891ff4a52d61e68f91a
Harsh M <52977247+harshmriduhash@users.noreply.github.com>
2025-10-13 18:01:04 +0530

    sample data

--- src/hook/useplandata.js
+++ src/hook/useplandata.js
@@ -2,7 +2,6 @@
 import { useEffect, useState } from "react";
 import { subscribeToPlanData } from "../services/plandata";
 
-
 const usePlanData = () => {
   const [planData, setPlanData] = useState(null);
 
